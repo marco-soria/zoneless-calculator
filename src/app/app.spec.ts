61a61ae4ecdf3942fb94293483c11716ac669f9a
marco-soria <marco.soriaa7@gmail.com>
2025-07-09 09:15:03 -0500

    tested calc service

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,17 +1,22 @@
 import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
       providers: [provideZonelessChangeDetection()],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -28,10 +33,47 @@ describe('App', () => {
     expect(result).toBe(3);
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(App);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it(`should have the 'zoneless-calculator' title`, () => {
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('zoneless-calculator');
+  });
+
+  it('should render router-outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
+  });
+
+  it('should render router-outlet wrapped with css classes', () => {
+    const divElement = compiled.querySelector('div');
+
+    const mustHaveClasses =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+    const divClasses = divElement?.classList.value.split(' ');
+
+    expect(divElement).not.toBeNull();
+
+    // divElement?.classList.forEach((className) => {
+    //   expect(mustHaveClasses).toContain(className);
+    // });
+
+    mustHaveClasses.forEach((className) => {
+      expect(divClasses).toContain(className);
+    });
+  });
+
+  //   it("should contain the 'buy me a beer' link ", () => {
+  //     const anchorElement = compiled.querySelector('a');
+
+  //     expect(anchorElement).not.toBeNull();
+  //     expect(anchorElement?.title).toBe('Buy me a beer');
+
+  //     expect(anchorElement?.href).toBe(
+  //       'https://www.buymeacoffee.com/scottwindon'
+  //     );
+  //     expect(anchorElement?.getAttribute('href')).toBe(
+  //       'https://www.buymeacoffee.com/scottwindon'
+  //     );
+  //   });
   // });
 });
