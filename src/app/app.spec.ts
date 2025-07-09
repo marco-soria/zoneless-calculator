df4d2427d41d268b8da2552d1412e782bd46f021
marco-soria <marco.soriaa7@gmail.com>
2025-07-08 21:01:21 -0500

    first test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -6,7 +6,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection()],
     }).compileComponents();
   });
 
@@ -16,10 +16,22 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it('should be 3', () => {
+    // A = Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    // A = Act
+    const result = num1 + num2;
+
+    // A = Assert
+    expect(result).toBe(3);
   });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(App);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  // });
 });
